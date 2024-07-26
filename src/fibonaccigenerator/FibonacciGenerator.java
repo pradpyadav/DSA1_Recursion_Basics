337db6632008f06f3e5a0deea38fe3a291c3bd14
Pradeep Yadav <pradpyadav@gmail.com>
2024-07-26 18:27:21 -0400

    Added fibonaccigenerator program

--- src/fibonaccigenerator/FibonacciGenerator.java
+++ src/fibonaccigenerator/FibonacciGenerator.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 
 public class FibonacciGenerator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Nth Fibonacci Generator Program:");
-		System.out.println("Enter the number to find the fibonacci value of it");
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Welcome to Nth Fibonacci Generator Program:");
+        System.out.println("Enter the number to find the fibonacci value of it");
 
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
 
-		try {
-			System.out.println("The : " + num + "th term in the fibonacci series is : " + generateNthFibonacci(num));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        try {
+            System.out.println("The : " + num + "th term in the fibonacci series is : " + generateNthFibonacci(num));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	static int generateNthFibonacci(int n) throws Exception {
-		if (n < 0) {
-			throw new Exception("Not a valid number");
+    static int generateNthFibonacci(int n) throws Exception {
+        if (n < 0) {
+            throw new Exception("Not a valid number");
 
-		} else if (n == 0 || n == 1) {
-			return 1;
-		} else {
-			return generateNthFibonacci(n - 1) + generateNthFibonacci(n - 2);
-		}
-	}
+        } else if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return generateNthFibonacci(n - 1) + generateNthFibonacci(n - 2);
+        }
+    }
 
 }
